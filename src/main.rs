e94320970fe96b15ec9e30e9e5c3483a20f3ed18
dwillie <david.willie.1@gmail.com>
2021-10-29 19:31:19 +1100

    W

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,21 @@ use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::rect::{Rect, Point};
-use std::time::Duration;
+use std::time::{Duration, SystemTime};
+use std::thread::sleep;
 use rand::prelude::*;
 use rayon::prelude::*;
 
-const GRID_WIDTH: usize = 700;
-const GRID_HEIGHT: usize = 350;
+const GRID_WIDTH: usize = 2000 ;
+const GRID_HEIGHT: usize = 1000;
 const GRID_LENGTH: usize = GRID_WIDTH * GRID_HEIGHT;
-const CELL_SIZE: u32 = 3;
+const CELL_SIZE: u32 = 1;
 const BG_COLOUR: Color = Color::RGB(0, 0, 0);
 const WINDOW_WIDTH: u32 = (GRID_WIDTH * CELL_SIZE as usize) as u32;
 const WINDOW_HEIGHT: u32 = (GRID_HEIGHT * CELL_SIZE as usize) as u32;
 const LIVE_VALUE: u8 = 250;
-const DEATH_STEP: u8 = 10;
-const FPS_TARGET: u32 = 30;
+const DEATH_STEP: u8 = 2;
+const FPS_TARGET: u32 = 60;
 
 fn val_to_colour(iu: &u8) -> Color {
     let i = *iu as f32 / (LIVE_VALUE as f32);
@@ -23,6 +24,10 @@ fn val_to_colour(iu: &u8) -> Color {
     let g = (50. * i).round() as u8;
     let b = (255. * i).round() as u8;
     return Color::RGB(r, g, b);
+}
+
+fn random_cell_state() -> u8 {
+    return if random::<bool>() { LIVE_VALUE } else { 0 };
 }
 
 fn coords_to_index(x: i32, y: i32) -> usize {
@@ -76,12 +81,12 @@ fn step_grid(in_grid: &[u8]) -> [u8; GRID_LENGTH] {
     });
 
     for x in 0..GRID_WIDTH {
-        out_grid[coords_to_index(x as i32, 0)] = if random::<bool>() { LIVE_VALUE } else { 0 };
-        out_grid[coords_to_index(x as i32, (GRID_HEIGHT - 1) as i32)] = if random::<bool>() { LIVE_VALUE } else { 0 };
+        out_grid[coords_to_index(x as i32, 0)] = random_cell_state();
+        out_grid[coords_to_index(x as i32, (GRID_HEIGHT - 1) as i32)] = random_cell_state();
     }
     for y in 0..GRID_HEIGHT {
-        out_grid[coords_to_index(0, y as i32)] = if random::<bool>() { LIVE_VALUE } else { 0 };
-        out_grid[coords_to_index((GRID_WIDTH - 1) as i32, y as i32)] = if random::<bool>() { LIVE_VALUE } else { 0 };
+        out_grid[coords_to_index(0, y as i32)] = random_cell_state();
+        out_grid[coords_to_index((GRID_WIDTH - 1) as i32, y as i32)] = random_cell_state();
     }
 
     return out_grid;
@@ -92,7 +97,6 @@ fn main() -> Result<(), String> {
 
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
-
     let window = video_subsystem.window("Game of Life", WINDOW_WIDTH, WINDOW_HEIGHT)
         .position_centered()
         .opengl()
@@ -110,9 +114,13 @@ fn main() -> Result<(), String> {
     let mut event_pump = sdl_context.event_pump()?;
     let mut grid = [0; GRID_LENGTH];
     for i in 0..GRID_LENGTH {
-        grid[i] = if random::<bool>() { LIVE_VALUE } else { 0 };
+        grid[i] = random_cell_state();
     }
+    let mut avg_frametime = 0.;
+    let mut frame_count = 0.;
     'running: loop {
+        let frame_start = SystemTime::now();
+        frame_count += 1.;
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
@@ -120,29 +128,36 @@ fn main() -> Result<(), String> {
                 },
                 Event::KeyDown { keycode: Some(Keycode::Space), .. } => {
                     for i in 0..GRID_LENGTH {
-                        grid[i] = if random::<bool>() { LIVE_VALUE } else { 0 };
+                        grid[i] = random_cell_state();
                     }
                 },
                 _ => {}
             }
         }
 
-        // 1 billion nanoseconds in a second, targeting 60fps.
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000 / FPS_TARGET));
+        sleep(Duration::new(0, 1_000_000_000 / FPS_TARGET));
 
         canvas.set_draw_color(BG_COLOUR);
         canvas.clear();
-
         grid.iter().enumerate().for_each(|(index, &i)| {
-            if i <= 1 { return; }
+            if i < LIVE_VALUE { return; }
+            // let draw_start = SystemTime::now();
             let (x, y) = index_to_coords(index);
-            let cell_colour = val_to_colour(&i);
-            canvas.set_draw_color(cell_colour);
+            canvas.set_draw_color(val_to_colour(&i));
             canvas.draw_point(Point::new(x as i32, y as i32)).unwrap();
+            // let draw_time_micros = SystemTime::now().duration_since(draw_start).unwrap().as_micros();
+            // if draw_time_micros > 0 {
+            //     println!("Draw took {} microseconds", draw_time_micros);
+            // }
         });
-
         canvas.present();
+
         grid = step_grid(&grid);
+        let frame_time = SystemTime::now().duration_since(frame_start).unwrap().as_nanos();
+        avg_frametime = ((frame_count * avg_frametime) + frame_time as f64) / (frame_count + 1.);
+        if frame_count > 500. {
+            println!("Avg frametime: {}ns", avg_frametime);
+        }
     }
 
     Ok(())
